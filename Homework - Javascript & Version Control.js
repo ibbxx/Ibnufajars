4ce080d4ae6533f0353c9c6183ac5a7f4c56c4e8
ibnu <ibnufajars99@gmail.com>
2023-09-09 23:45:51 +0800

    first commit

---
+++ Homework - Javascript & Version Control.js
@@ -0,0 +1,114 @@
+// Fungsi untuk membuat array dengan nilai acak antara 1 dan 50
+function buatArrayRandom(panjangArray) {
+    let nilaiRandom = [];
+    for (let i = 0; i < panjangArray; i++) {
+      nilaiRandom.push(Math.floor(Math.random() * 50) + 1);
+    }
+    return nilaiRandom;
+  }
+  
+  // Fungsi untuk memisahkan array menjadi array genap dan ganjil
+  function pisahArrayGenapGanjil(array) {
+    let arrayGenap = [];
+    let arrayGanjil = [];
+    for (let i = 0; i < array.length; i++) {
+      if (i % 2 === 0) {
+        arrayGenap.push(array[i]);
+      } else {
+        arrayGanjil.push(array[i]);
+      }
+    }
+    return { arrayGenap, arrayGanjil };
+  }
+  
+  // Fungsi untuk menghitung nilai minimum
+  function hitungMin(array) {
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+    return min;
+  }
+  
+  // Fungsi untuk menghitung nilai maksimum
+  function hitungMax(array) {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+    return max;
+  }
+  
+  // Fungsi untuk menghitung total
+  function hitungTotal(array) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+  
+  // Fungsi untuk menghitung rata-rata
+  function hitungRataRata(array) {
+    return hitungTotal(array) / array.length;
+  }
+  
+  // Fungsi untuk membandingkan array genap dan ganjil
+  function bandingkanArray(arrayGenap, arrayGanjil) {
+    const minGenap = hitungMin(arrayGenap);
+    const maxGanjil = hitungMax(arrayGanjil);
+    const totalGenap = hitungTotal(arrayGenap);
+    const totalGanjil = hitungTotal(arrayGanjil);
+    const rataRataGenap = hitungRataRata(arrayGenap);
+    const rataRataGanjil = hitungRataRata(arrayGanjil);
+  
+    return {
+      min: minGenap > minGanjil ? "Min lebih besar array genap" : "Min lebih besar array ganjil",
+      max: maxGanjil > maxGenap ? "Max lebih besar array ganjil" : "Max lebih besar array genap",
+      total: totalGenap === totalGanjil ? "Total memiliki nilai sama antara array genap dan ganjil" : "Total berbeda antara array genap dan ganjil",
+      rataRata: rataRataGanjil > rataRataGenap ? "Rata rata lebih besar array ganjil" : "Rata rata lebih besar array genap"
+    };
+  }
+  
+  // Membuat array dengan nilai acak
+  const panjangArray = 100;
+  const nilaiAcak = buatArrayRandom(panjangArray);
+  
+  // Memisahkan array menjadi genap dan ganjil
+  const { arrayGenap, arrayGanjil } = pisahArrayGenapGanjil(nilaiAcak);
+  
+  // Menghitung nilai min, max, total, dan rata-rata untuk array genap dan ganjil
+  const minGenap = hitungMin(arrayGenap);
+  const maxGenap = hitungMax(arrayGenap);
+  const totalGenap = hitungTotal(arrayGenap);
+  const rataRataGenap = hitungRataRata(arrayGenap);
+  
+  const minGanjil = hitungMin(arrayGanjil);
+  const maxGanjil = hitungMax(arrayGanjil);
+  const totalGanjil = hitungTotal(arrayGanjil);
+  const rataRataGanjil = hitungRataRata(arrayGanjil);
+  
+  // Membandingkan array genap dan ganjil
+  const perbandingan = bandingkanArray(arrayGenap, arrayGanjil);
+  
+  // Menampilkan hasil
+  console.log("Array dengan jumlah index", panjangArray + ":", nilaiAcak);
+  console.log("Array genap dengan jumlah index", arrayGenap.length + ":", arrayGenap);
+  console.log("Array ganjil dengan jumlah index", arrayGanjil.length + ":", arrayGanjil);
+  console.log("Min, Max, Total, Rata-rata pada array genap:");
+  console.log("Min:", minGenap);
+  console.log("Max:", maxGenap);
+  console.log("Total:", totalGenap);
+  console.log("Rata-rata:", rataRataGenap);
+  console.log("Min, Max, Total, Rata-rata pada array ganjil:");
+  console.log("Min:", minGanjil);
+  console.log("Max:", maxGanjil);
+  console.log("Total:", totalGanjil);
+  console.log("Rata-rata:", rataRataGanjil);
+  console.log("Perbandingan nilai:");
+  console.log(perbandingan);
+  
